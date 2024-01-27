5c04e8403d054a1534a878f88f43edd557647520
LeafYeeXYZ <leafyeexyz@outlook.com>
2024-01-27 19:31:44 +0800

    完成

---
+++ main.js
@@ -0,0 +1,53 @@
+const forms = document.querySelectorAll('form')
+const submitBtn = document.querySelector('button')
+submitBtn.addEventListener('click', submitForm)
+function submitForm() {
+  console.log(forms)
+  let score1 = 0
+  let score2 = 0
+  let score3 = 0
+  let score4 = 0
+  let totalScore = 0
+  for (let i = 0; i < forms.length; i++) {
+    
+    if (i < 5) {
+      score1 += +forms[i].querySelector('input:checked').value
+    } else if (i < 9) {
+      score2 += +forms[i].querySelector('input:checked').value
+    } else if (i < 14) {
+      score3 += +forms[i].querySelector('input:checked').value
+    } else {
+      score4 += +forms[i].querySelector('input:checked').value
+    }
+  }
+  totalScore = ((score1 + score2 + score3 + score4) / 18 - 4.40) / 0.58
+  score1 = (score1 / 5 - 3.13) / 1.15
+  score2 = (score2 / 4 - 5.35) / 0.96
+  score3 = (score3 / 5 - 5.35) / 0.83
+  score4 = (score4 / 4 - 3.85) / 1.20
+  console.log(totalScore, score1, score2, score3, score4)
+  alert(`
+    在在校学生这一群体中
+    您的内卷程度总分超过了${Math.round(fromZtoP(totalScore))}%的人
+    其中：
+    心理压力维度：超过了${Math.round(fromZtoP(score1))}%的人
+    社会规范维度：超过了${Math.round(fromZtoP(score2))}%的人
+    竞争行为维度：超过了${Math.round(fromZtoP(score3))}%的人
+    资源稀缺维度：超过了${Math.round(fromZtoP(score4))}%的人
+  `)
+}
+// 定义一个函数，用来计算正态分布函数的近似值
+function fromZtoP (z) {
+  let result = 0.5 * (1 + math.erf(z / Math.sqrt(2))) // P 值
+  result = result * 100 // 百分比
+  result = result.toFixed(2) // 保留两位小数
+  return result
+}
+
+
+
+
+
+
+
+
